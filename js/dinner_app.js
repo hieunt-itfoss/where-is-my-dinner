612bf8932ec4de92ba2ec2cdcd7f52ce2b2a2142
Nguyen Tri Hieu <nhieu@fossil.com>
2022-12-14 10:43:49 +0700

    update indicator when schedule time is set

--- js/dinner_app.js
+++ js/dinner_app.js
@@ -3,6 +3,8 @@
 let timeElements = [];
 const dayInMins = 1440;
 
+const scheduleButton = document.getElementById("setTimer");
+
 // Initialize scheduleTime that stores user input time
 let scheduleTime = new Date().setHours(13, 30, 0, 0);
 
@@ -29,21 +31,36 @@ function clearAllAlarms() {
 }
 
 // -------------- SCHEDULER --------------
-function updateScheduleTime() {
+async function updateScheduleTime() {
   // Parse time elements from input-value
   timeElements = document.getElementById("appt").value.split(":");
 
-  // Set new time value for reminder
-  scheduleTime = new Date().setHours(timeElements[0], timeElements[1], 0, 0);
+  try {
+    // Set new time value for reminder
+    scheduleTime = new Date().setHours(timeElements[0], timeElements[1], 0, 0);
 
-  // Clear the older alarm if it exists
-  clearAlarm("dinner");
+    // Clear the older alarm if it exists
+    clearAlarm("dinner");
 
-  // Set an alarms trigger on scheduled timestamps
-  createAlarm("dinner", scheduleTime, dayInMins);
+    // Set an alarms trigger on scheduled timestamps
+    createAlarm("dinner", scheduleTime, dayInMins);
+
+    // Update button value to indicate that the alarm has been set
+    scheduleButton.innerText = "Update Success";
+
+  } catch (e) {
+    console.log(e);
+  } finally {
+    // Change the button value to default after 2 sec
+    // TODO: Apply dynamic content for button value
+    await delay(2000);
+    scheduleButton.innerText = "Set schedule time";
+  }
 }
 
 // -------------- EVENT TRIGGER --------------
 document.getElementById("quickOpen").addEventListener("click", openForm);
-document.getElementById("setTimer").addEventListener("click", updateScheduleTime);
-document.getElementById("clearAlarm").addEventListener("click", clearAlarm("dinner"));
+scheduleButton.addEventListener("click", updateScheduleTime);
+document
+  .getElementById("clearAlarm")
+  .addEventListener("click", clearAlarm("dinner"));
